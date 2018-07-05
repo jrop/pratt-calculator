9317d1b740db5b7ed7f9db35073c580a55784eb8
Jonathan Apodaca <jrapodaca@gmail.com>
2018-07-05 15:38:01 -0600

    Simplify tests

--- test.js
+++ test.js
@@ -1,67 +1,22 @@
-const test = require('tape')
+const _test = require('tape')
 const {calc} = require('./parser')
 
-test('1 + 2 * 3', t => {
-	t.equal(calc('1 + 2 * 3'), 7)
-	t.end()
-})
+const test = (expr, expected) =>
+	_test(expr, t => {
+		t.equal(calc(expr), expected)
+		t.end()
+	})
 
-test('(1 + 2) * 3', t => {
-	t.equal(calc('(1 + 2) * 3'), 9)
-	t.end()
-})
-
-test('1', t => {
-	t.equal(calc('1'), 1)
-	t.end()
-})
-
-test('((1))', t => {
-	t.equal(calc('((1))'), 1)
-	t.end()
-})
-
-test('3^4^.5', t => {
-	t.equal(calc('3^4^.5'), 9)
-	t.end()
-})
-
-test('(2^3)^4', t => {
-	t.equal(calc('(2^3)^4'), 4096)
-	t.end()
-})
-
-test('-2*-2', t => {
-	t.equal(calc('-2*-2'), 4)
-	t.end()
-})
-
-test('-2*2', t => {
-	t.equal(calc('-2*2'), -4)
-	t.end()
-})
-
-test('5/2/.5', t => {
-	t.equal(calc('5/2/.5'), 5)
-	t.end()
-})
-
-test('5 - 3 - 1 - 4', t => {
-	t.equal(calc('5 - 3 - 1 - 4'), -3)
-	t.end()
-})
-
-test('floor(ceil(0.5) / 2)', t => {
-	t.equal(calc('floor(ceil(0.5) / 2)'), 0)
-	t.end()
-})
-
-test('PI', t => {
-	t.equal(calc('PI'), Math.PI)
-	t.end()
-})
-
-test('abs(cos(PI)) + 9', t => {
-	t.equal(calc('abs(cos(PI)) + 9'), 10)
-	t.end()
-})
+test('1 + 2 * 3', 7)
+test('(1 + 2) * 3', 9)
+test('1', 1)
+test('((1))', 1)
+test('3^4^.5', 9)
+test('(2^3)^4', 4096)
+test('-2*-2', 4)
+test('-2*2', -4)
+test('5/2/.5', 5)
+test('5 - 3 - 1 - 4', -3)
+test('floor(ceil(0.5) / 2)', 0)
+test('PI', Math.PI)
+test('abs(cos(PI)) + 9', 10)
